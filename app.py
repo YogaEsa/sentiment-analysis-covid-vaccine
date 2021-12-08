f4488c03b35d1fba8d68c74625a71b02cba58dba
Yoga Esa <me.yogaesa@gmail.com>
2021-12-08 16:28:06 +0700

    add data tweet

--- app.py
+++ app.py
@@ -2,7 +2,33 @@
 
 from flask import Flask, render_template, url_for, request
 
-# ML PACKAGES
+# ML PACKAGES LIB
+import nltk
+import string
+import re
+import joblib
+import pickle
+import csv
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from Sastrawi.StopWordRemover.StopWordRemoverFactory import StopWordRemoverFactory
+from sklearn import model_selection
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.multiclass import OneVsRestClassifier
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.metrics import classification_report
+from sklearn.metrics import accuracy_score
+from imblearn.pipeline import Pipeline, make_pipeline
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.naive_bayes import MultinomialNB
+from imblearn.over_sampling import SMOTE
+from sklearn.model_selection import train_test_split
+from imblearn.over_sampling import BorderlineSMOTE
 
 # In[ ]:
 
@@ -22,7 +48,8 @@ def klasifikasi():
 
 @app.route('/data')
 def data():
-    return render_template('data.html')
+    data = pd.read_csv("data/data_tweets.csv", delimiter=';')
+    return render_template('data.html',data=data)
 
 
 if __name__ == '__main__':
